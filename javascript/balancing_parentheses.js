427bab98914e6553b5ac6b6d469ba514f1d3a130
Morgan Byrne <79115095+mo-hobbs@users.noreply.github.com>
2022-04-19 13:10:18 -0400

    tests pass

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,5 +1,21 @@
+// for every opening ( paren there must be a closing )
+// return the number of additional parentheses needed for the string to be considered balanced
+// create one string for opening, one for closing and compare  string length and return the difference 
+
 function balancingParentheses(string) {
   // type your code here
+  let left = ''
+  let right = ''
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "(") {
+      left+=string[i]
+    }
+    else if (string[i] === ")") {
+      right+=string[i];
+    }
+  }
+ return Math.abs(left.length - right.length);
 }
 
 if (require.main === module) {
